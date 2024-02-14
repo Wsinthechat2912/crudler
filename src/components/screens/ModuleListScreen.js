f549a96aab12cfc772f6f7e69df787df28265233
Wsinthechat2912 <k2125891@kingston.ac.uk>
2024-02-14 21:18:57 +0000

    Changes made to Module Screen Page

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,21 +1,28 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
+import ModuleList from '../entity/modules/ModuleList.js';
+
+import initialModules from '../../data/modules.js';
 
 const ModuleListScreen = () => {
 // Initialisations ---------------------
+const modules = initialModules;
+
 // State -------------------------------
 // Handlers ----------------------------
-// View --------------------------------
+const handleSelect = (module) => alert(`Item ${module.ModuleCode} Selected`);
 
+// View --------------------------------
   return (
     <Screen>
-
-      <Text>List</Text>
-
+      <ModuleList modules={modules} onSelect={handleSelect} />
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
 
 export default ModuleListScreen;
+
