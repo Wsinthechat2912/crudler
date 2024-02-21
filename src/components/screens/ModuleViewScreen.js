eea23b6d16d40a32ed96a6e11251e3b997a93f42
Wsinthechat2912 <k2125891@kingston.ac.uk>
2024-02-21 19:14:04 +0000

    Making delete button work
    Making the delete work in the module view page.

--- src/components/screens/ModuleViewScreen.js
+++ src/components/screens/ModuleViewScreen.js
@@ -4,14 +4,16 @@ import ModuleView from "../entity/modules/ModuleView";
 
 const ModuleViewScreen = ({ navigate, route }) => {
   // Initialisations ---------------------
-  const { module } = route.params;
+  const { module, onDelete } = route.params;
   // State -------------------------------
   // Handlers ----------------------------
   // View --------------------------------
 
-  return <Screen>
-    <ModuleView module={module} />
-  </Screen>;
+  return (
+    <Screen>
+      <ModuleView module={module} onDelete={onDelete} />
+    </Screen>
+  );
 };
 
 const styles = StyleSheet.create({});
