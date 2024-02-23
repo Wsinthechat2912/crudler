5b58d6e80ac9d113349d33ad6492da4dba3ddde7
Wsinthechat2912 <k2125891@kingston.ac.uk>
2024-02-23 19:47:05 +0000

    Update Screen.js

--- src/components/layout/Screen.js
+++ src/components/layout/Screen.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
-const Screen = ({children}) => {
-// Initialisations ---------------------
-// State -------------------------------
-// Handlers ----------------------------
-// View --------------------------------
+const Screen = ({ children }) => {
+  // Initialisations ---------------------
+  // State -------------------------------
+  // Handlers ----------------------------
+  // View --------------------------------
 
   return (
     <View style={styles.screen}>
@@ -13,13 +13,13 @@ const Screen = ({children}) => {
       <StatusBar style="light" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     padding: 15,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
 
